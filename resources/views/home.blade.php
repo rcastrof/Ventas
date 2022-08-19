56d9e51c539596d04ceee3bb5e41ae65f1002f0d
Rodrigo Castro <rodrigocastro101998@gmail.com>
2022-08-19 09:07:51 -0400

    header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Ultimos objetos creados') }}</div>
+                    <div class="card-header">{{ __(' objetos creados') }}</div>
 
                     <div class="card-body">
                         @if (session('status'))
